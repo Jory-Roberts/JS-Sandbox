b1d19aa67f1f02ae8f9a895c20c315f1dfc5cece
Jory Roberts <robertsjory13@gmail.com>
2023-05-01 12:10:36 -0500

    adds CSS styling

---
+++ wordgame.js
@@ -0,0 +1,35 @@
+const tileDisplay = document.querySelector('.tile-container');
+console.log(tiles);
+const keyboard = document.querySelector('.key-container');
+console.log(keys);
+
+const keys = [
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'ENTER',
+    'Z',
+    'W',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M',
+    '<',
+];
