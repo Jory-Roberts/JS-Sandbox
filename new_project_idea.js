5d5ea406495db234b07835d915c10c304c445021
Jory Roberts <robertsjory13@gmail.com>
2023-05-10 12:23:26 -0500

    adds card with text content

--- new_project_idea.js
+++ new_project_idea.js
@@ -1,1 +1,18 @@
+const person = {
+    fullName: {
+        'First Name': 'Bob',
+        'Last Name': 'Made Up',
+        Occupation: 'Software Engineer',
+    },
+};
 
+const addCardContent = (person) => {
+    const card = document.querySelector('.card');
+    const paragraph = document.createElement('p');
+    paragraph.id = 'paragraph';
+    paragraph.textContent = person.fullName['First Name'];
+    card.appendChild(paragraph);
+    console.log(paragraph);
+};
+
+addCardContent(person);
